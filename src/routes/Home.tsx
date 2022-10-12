ea7def3e2e80b0217e1bff6d17e4dae75e31d8e1
June Roh <june.roh@streetsmarket.com>
2022-10-12 09:25:08 -0400

    show rooms from fetched data

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,70 +1,87 @@
 import {
-  Box,
-  Button,
-  Grid,
-  Heading,
-  HStack,
-  Image,
-  Skeleton,
-  SkeletonText,
-  Text,
-  VStack,
+    Box,
+    Button,
+    Grid,
+    Heading,
+    HStack,
+    Image,
+    Skeleton,
+    SkeletonText,
+    Text,
+    VStack,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { FaRegHeart, FaStar } from "react-icons/fa";
 import Room from "../components/Room";
 import RoomSkeleton from "../components/RoomSkeleton";
 
+interface IPhoto {
+    pk: number;
+    file: string;
+    description: string;
+}
+interface IRoom {
+    pk: number;
+    name: string;
+    country: string;
+    city: string;
+    price: number;
+    rating: number;
+    is_owner: boolean;
+    photos: IPhoto[];
+}
+
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [rooms, setRooms] = useState([]);
-  const fetchRooms = async () => {
-    const response = await fetch("http://localhost:8000/api/v1/rooms/");
-    const json = await response.json();
-    setRooms(json);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    fetchRooms();
-  }, []);
-  return (
-    <Grid
-      mt={10}
-      px={{
-        base: 10,
-        lg: 40,
-      }}
-      columnGap={4}
-      rowGap={8}
-      templateColumns={{
-        sm: "1fr", //phone
-        md: "repeat(2, 1fr)",
-        lg: "repeat(3, 1fr)",
-        xl: "repeat(4, 1fr)",
-        "2xl": "repeat(5, 1fr)",
-      }}
-    >
-      {isLoading ? (
-        <>
-          <RoomSkeleton />
-          <RoomSkeleton />
-          <RoomSkeleton />
-          <RoomSkeleton />
-          <RoomSkeleton />
-          <RoomSkeleton />
-          <RoomSkeleton />
-          <RoomSkeleton />
-        </>
-      ) : null}
-      {rooms.map((room) => (
-        <Room
-          name={room.name}
-          rating={room.rating}
-          city={room.city}
-          country={room.country}
-          price={room.price}
-        />
-      ))}
-    </Grid>
-  );
+    const [isLoading, setIsLoading] = useState(true);
+    const [rooms, setRooms] = useState<IRoom[]>([]);
+    const fetchRooms = async () => {
+        const response = await fetch("http://localhost:8000/api/v1/rooms/");
+        const json = await response.json();
+        setRooms(json);
+        setIsLoading(false);
+    };
+    useEffect(() => {
+        fetchRooms();
+    }, []);
+    return (
+        <Grid
+            mt={10}
+            px={{
+                base: 10,
+                lg: 40,
+            }}
+            columnGap={4}
+            rowGap={8}
+            templateColumns={{
+                sm: "1fr", //phone
+                md: "repeat(2, 1fr)",
+                lg: "repeat(3, 1fr)",
+                xl: "repeat(4, 1fr)",
+                "2xl": "repeat(5, 1fr)",
+            }}
+        >
+            {isLoading ? (
+                <>
+                    <RoomSkeleton />
+                    <RoomSkeleton />
+                    <RoomSkeleton />
+                    <RoomSkeleton />
+                    <RoomSkeleton />
+                    <RoomSkeleton />
+                    <RoomSkeleton />
+                    <RoomSkeleton />
+                </>
+            ) : null}
+            {rooms.map((room) => (
+                <Room
+                    imageUrl={room.photos[0].file}
+                    name={room.name}
+                    rating={room.rating}
+                    city={room.city}
+                    country={room.country}
+                    price={room.price}
+                />
+            ))}
+        </Grid>
+    );
 }
